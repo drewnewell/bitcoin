f3aac357b140f286b4c732e54f4040cf21c90f69
Drew Newell <drewnewell@gmail.com>
2019-10-15 11:33:35 -0500

    wip - just grab headers for data

--- make_data.py
+++ make_data.py
@@ -1,37 +1,44 @@
-from __future__ import print_function
-
-from pifkoin.blockchain import BlockHeader
+import click
 import pickle
 import struct
 
+from pifkoin.blockchain import BlockHeader
+
 
 OUTPUT_FILE = 'data/nonce.pkl'
-START_HEIGHT = 400000
 BITCOIN_CONF_FILE = '/home/drew/archive/bitcoin/bitcoin.conf'
 
 
-def main():
+@click.command()
+@click.option('--output', help='output file', default=OUTPUT_FILE)
+@click.option('--height', help='starting height', type=int)
+@click.option('--size', help='number of blocks to extract', type=int, default=1000)
+@click.option('--conf', help='configuration file', default=BITCOIN_CONF_FILE)
+def extract(output, height, size, conf):
 
     def get_blockheader(height):
-        return BlockHeader.from_blockchain(height=height, config_filename=BITCOIN_CONF_FILE)
+        return BlockHeader.from_blockchain(height=height, config_filename=conf)
 
     last_block = get_blockheader(-1)
-    current_height = last_block.height
+    if height is not None:
+        current_height = height
+    else:
+        current_height = last_block.height
+    print('starting with block {}'.format(current_height))
 
-    output = []
-    for i in range(current_height - START_HEIGHT):
-        print('processing block {}...\r'.format(START_HEIGHT + i), end='')
-        header = get_blockheader(START_HEIGHT + i)
+    data = []
+    for i in range(size):
+        print('processing block {}...\r'.format(current_height - i), end='')
+        header = get_blockheader(current_height - i)
         header_bytes = header.bytes
-        parital_bytes = header_bytes[:-4]
-        nonce = header_bytes[-4:]
+        #parital_bytes = header_bytes[:-4]
+        #nonce = header_bytes[-4:]
 
-        output.append((header.height, parital_bytes, nonce))
+        data.append(header_bytes)
 
-    with open(OUTPUT_FILE, 'wb') as f:
-        pickle.dump(output, f)
+    with open('data/' + output, 'wb') as f:
+        pickle.dump(data, f)
 
 
 if __name__ == '__main__':
-    main()
-
+    extract()
